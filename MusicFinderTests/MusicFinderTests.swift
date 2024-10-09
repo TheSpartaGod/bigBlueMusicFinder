cbdccdc29ee31820c0c16b54698ff641beb9f4b4
Aqshal Wibisono <axlwibisono@gmail.com>
2024-10-09 21:45:25 +0700

    Initial Commit

---
+++ MusicFinderTests/MusicFinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicFinderTests.swift
+//  MusicFinderTests
+//
+//  Created by Aqshal Wibisono on 09/10/24.
+//
+
+import Testing
+@testable import MusicFinder
+
+struct MusicFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
